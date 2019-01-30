ceaab84621d2b28395713c8490d2589604bf9b41
Brenda <odonnell.brenda@gmail.com>
2019-01-30 15:47:18 +0000

    Completed Ex10. The interactive menu calls a print menu method and a process method. The process method executes commands based on the user's input.

--- Ex10.rb
+++ Ex10.rb
@@ -1,60 +1,50 @@
 @students = []
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    # create an empty array
-    students = []
-    # get the first name
     name = gets.chomp
-    # while the name is not empty, repeat this code
     while !name.empty? do
-        # add the student has to the array
-        students << {name: name, cohort: :november}
-        puts "Now we have #{students.count} students"
-        # get another name from the user
+        @students << {name: name, cohort: :november}
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
-    # return the array of students
-    students
 end
-
-students = input_students
 
 def print_header
     puts "The students of Villains Academy"
-puts "---------------"
+    puts "---------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print_student_list
+  @students.each_with_index do |student, index|
       puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-    puts "Overall we have #{students.count} great students"
+def print_footer
+    puts "Overall we have #{@students.count} great students"
 end
 
-
 def interactive_menu
-  # @students = []
   loop do
-    # Call DEF PRINT MENU
     print_menu
-    selection = gets.chomp
-    # 3. do what the user has asked
+    process(gets.chomp)
+  end
+end    
+
+
+def process(selection)
     case selection
     when "1"
-      @students = input_students
+      input_students   # calling the input students method
     when "2"
-    # Call DEF SHOW STUDENTS
-    show_students
+      show_students  # calling the show_students method
     when "9"
-      exit # this will cause the program to terminate
+      exit
     else
       puts "I don't know what you meant, try again"
     end
-  end
 end
 
 def print_menu
@@ -63,14 +53,15 @@ def print_menu
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 
+
 def show_students
   print_header
-  print(students)
-  print_footer(students)
+  print_student_list
+  print_footer
 end
 
 
 interactive_menu
 print_header
-print(students)
-print_footer(students)
+print_student_list
+print_footer
