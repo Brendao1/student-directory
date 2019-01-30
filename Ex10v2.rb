b4d20441db59388f1a42f69deab44d9576c13397
Brenda <odonnell.brenda@gmail.com>
2019-01-30 14:58:48 +0000

    Cleaned the code up.

--- Ex10v2.rb
+++ Ex10v2.rb
@@ -3,19 +3,16 @@
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    # create an empty array
-    # students = []
-    # get the first name
+    
     name = gets.chomp
-    # while the name is not empty, repeat this code
+
     while !name.empty? do
         # add the student has to the array
         @students << {name: name, cohort: :november}
         puts "Now we have #{@students.count} students"
-        # get another name from the user
+        
         name = gets.chomp
     end
-    # return the array of students
     @students
 end
 
@@ -39,18 +36,18 @@ end
 def interactive_menu
   # @students = []
   loop do
-    # Call DEF PRINT MENU
+
     print_menu
     selection = gets.chomp
-    # 3. do what the user has asked
+
     case selection
     when "1"
       @students = input_students
     when "2"
-    # Call DEF SHOW STUDENTS
+
     show_students
     when "9"
-      exit # this will cause the program to terminate
+      exit
     else
       puts "I don't know what you meant, try again"
     end
@@ -60,7 +57,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll be adding more items  
+  puts "9. Exit" 
 end
 
 
