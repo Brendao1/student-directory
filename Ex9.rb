4e07dd59713d53718fd7b34d2c71194c5df23d06
Brenda <odonnell.brenda@gmail.com>
2019-01-30 14:31:06 +0000

    Correction to comment on print method: this prints all students, not only those 12 chars or less

--- Ex9.rb
+++ Ex9.rb
@@ -24,7 +24,6 @@ def print_header
 puts "---------------"
 end
 
-# Question 3: print only students whose name is shorter than 12 characters.
 def print(students)
   students.each_with_index do |student, index|
       puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
