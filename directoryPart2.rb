af1198d6a5c0116de53d0db4009d76d476c83313
Brenda <odonnell.brenda@gmail.com>
2019-01-31 13:01:44 +0000

    Interactive menu completed

--- directoryPart2.rb
+++ directoryPart2.rb
@@ -10,8 +10,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -19,14 +19,27 @@ def load_students
   file.close
 end
 	
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+	
+	
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    name = gets.chomp
+    name = STDIN.gets.chomp
     while !name.empty? do
         @students << {name: name, cohort: :november}
         puts "Now we have #{@students.count} students"
-        name = gets.chomp
+        name = STDIN.gets.chomp
     end
 end
 
@@ -48,7 +61,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end    
 
@@ -85,8 +98,5 @@ def show_students
   print_footer
 end
 
-
+try_load_students
 interactive_menu
-print_header
-print_student_list
-print_footer
