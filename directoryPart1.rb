b605c5f8ecd921882f4cb7019ff5f03f9f79464e
Brenda <odonnell.brenda@gmail.com>
2019-01-31 11:21:16 +0000

    first part of the directory exercises before the interactive menu

---
+++ directoryPart1.rb
@@ -0,0 +1,88 @@
+@center = 100
+#@students = []
+
+def input_students
+    puts "Enter the name of the student".center(@center)
+    puts "To finish, just hit return twice".center(@center)
+    students = []
+    
+    name = gets.rstrip
+    while !name.empty? do
+        puts "What is their nationality"
+        nationality = gets.chomp
+        puts "Which cohort?"
+        cohort = add_cohort
+        students << {name: name, nationality: nationality, cohort: cohort.to_sym}
+        if students.count == 1
+            puts "Now we have #{students.count} student. Next student"
+        else
+            puts "Now we have #{students.count} students. Next student"
+        end
+        # get another name from the user. Still using chomp in this instance
+        name = gets.chomp
+    end
+    students
+end
+
+
+def add_cohort
+    cohort_arr = {1 => :January, 2 => :February, 3 => :March, 4 => :April, 5 => :May, 
+    6 => :June, 7 => :July, 8 => :August, 9 => :Septmeber, 10 => :October, 11 => :November, 12 => :December, 13 => :tbc}
+    puts "Enter month (1 to 12) of the cohort to join: "
+    num = gets.strip.to_i
+    if cohort_arr.has_key?num
+        cohort = cohort_arr[num]
+       else
+        cohort = cohort_arr[13]
+    end
+    cohort
+end
+
+students = input_students
+
+def print_header
+    puts "The students of Villains Academy".center(@center)
+    puts "---------------".center(@center)
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+      puts "#{index+1} #{student[:name]} #{student[:nationality]} #{student[:dob]} (#{student[:cohort]} cohort)".center(@center)
+  end
+end
+
+=begin
+def print_by_cohort(students)
+    puts "Enter the month to group by: ".center(@center)
+    if !students.empty?
+      cohorts = students.map
+      sorted_by_cohort = {}
+      month = gets.strip.to_sym
+      cohorts.each do |student|
+        cohort = student[:cohort]
+        name = student[:name]
+        if sorted_by_cohort[cohort] == nil
+            sorted_by_cohort = [name]
+        else
+            sorted_by_cohort[cohort].push(name)
+        end
+      end
+    puts sorted_by_cohort[month]
+  else
+end
+end
+=end    
+
+def print_footer(students)
+    if students.count == 1
+    puts "Overall we have #{students.count} great student".center(@center)
+    else
+    puts "Overall we have #{students.count} great students".center(@center)    
+    end    
+end
+
+print_header
+print(students)
+print_footer(students)
+#print_by_cohort(students)
+
